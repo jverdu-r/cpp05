0f6edba6c11d3fb41329b4975554ad2e5ec0a748
jverdu-r <jorge.verdu.ruiz@gmail.com>
2024-11-06 17:34:45 +0100

    added 2nd form to ex02

---
+++ ex02/src/RobotomyRequestForm.cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jverdu-r <jverdu-r@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 16:22:51 by jverdu-r          #+#    #+#             */
+/*   Updated: 2024/11/06 17:31:37 by jverdu-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm(void): AForm("RobotomyRequestForm", 72, 45)
+{
+    this->_target = "none";
+}
+
+RobotomyRequestForm::~RobotomyRequestForm(void){}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string target): AForm("RobotomyRequestForm", 72, 45)
+{
+    this->_target = target;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm const &origin): AForm("RobotomyRequestForm", 72, 45)
+{
+    this->_target = origin._target;
+}
+
+std::string RobotomyRequestForm::getTarget(void)
+{
+    return(this->_target);
+}
+
+void RobotomyRequestForm::action(void) const
+{
+    std::srand(std::time(0));
+    int roboLuck = std::rand() % 2;
+    if (roboLuck > 0)
+        std::cout << "\033[32m" << this->_target << " has been robotomized" <<  "\033[0m" << std::endl;
+    else
+        std::cout << "\033[33m" << this->_target << " hasn't been robotomized" <<  "\033[0m" << std::endl;
+}
